0c60ef43605d3e676d5f7a5c6a3b5ca887336864
your name hereEddy_Marshall <epmarsha2@gmail.com>
2021-08-13 22:45:15 -0400

    create mouseover event listeners

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import Guitar from "./scripts/guitar"
-import Scale from "./scripts/scale"
-import guitarString from "./scripts/guitarstring"
 
 
 document.addEventListener("DOMContentLoaded", ()=>{
@@ -10,8 +8,18 @@ document.addEventListener("DOMContentLoaded", ()=>{
     const guitar1 = new Guitar(body, "G");
     const guitar2 = new Guitar(body, "Bb");
 
-    console.log(guitar1);
-    console.log(guitar2);
+    let strings = document.getElementsByClassName("guitarstring")
+    for (let i = 0; i < strings.length; i++) {
+        strings[i].addEventListener("mouseover", (event) => {
+            event.target.style.setProperty("--noteOpacity", 1)
+        })
+        strings[i].addEventListener("mouseout", (event) => {
+            event.target.style.setProperty("--noteOpacity", 0)
+        })
+
+    }
+    
+
 
 })
 
